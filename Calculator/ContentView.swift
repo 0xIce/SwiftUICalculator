49732ecf64514c19d5449ee9be273176e589d512
hongqima <hongqima@yangqianguan.com>
2019-06-06 13:16:40 +0800

    init

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by 马红奇 on 2019/6/6.
+//  Copyright © 2019 NSHotchner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
